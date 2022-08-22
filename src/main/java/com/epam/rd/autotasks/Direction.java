d9c511e64d98cbb176f605b1bf975c3cdd43a68d
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-08-22 11:53:52 +0300

    t

--- src/main/java/com/epam/rd/autotasks/Direction.java
+++ src/main/java/com/epam/rd/autotasks/Direction.java
@@ -8,22 +8,73 @@ public enum Direction {
     Direction(final int degrees) {
         this.degrees = degrees;
     }
-
-    private int degrees;
+public int getDegrees() {
+        return degrees;
+}
+    private final int degrees;
 
     public static Direction ofDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        int degreesFinal = degrees;
+
+        if (degrees < 0) {
+              degreesFinal = 360 + degrees;
+        }
+            else if (degrees >= 360) {
+                degreesFinal = degrees % 360;
+        }
+        Direction[] values = Direction.values();
+        for (Direction value : values) {
+            if (value.getDegrees() == degreesFinal) {
+                return value;
+            }
+        }
+        return null;
     }
 
+
+
     public static Direction closestToDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        int degreesFinal = degrees;
+
+        if (degrees < 0) {
+            degreesFinal = 360 + degrees;
+        } else if (degrees >= 360) {
+            degreesFinal = degrees % 360;
+        }
+        Direction[] values = Direction.values();
+        for (Direction value : values) {
+            if (value.getDegrees() == degreesFinal || value.getDegrees()+22 >= degreesFinal) {
+                return value;
+            }
+            }
+        return null;
     }
 
     public Direction opposite() {
-        throw new UnsupportedOperationException();
+        int degreesFinal = degrees;
+
+        if (degrees < 0) {
+            degreesFinal = 360 + degrees;
+        } else if (degrees >= 360) {
+            degreesFinal = degrees % 360;
+        }
+        Direction[] values = Direction.values();
+        for (Direction value : values) {
+            if (value.getDegrees()+180 == degreesFinal || value.getDegrees()-180 == degreesFinal) {
+                return value;
+            }
+        }
+        return null;
     }
 
+
     public int differenceDegreesTo(Direction direction) {
-        throw new UnsupportedOperationException();
+        Direction dir = this;
+        if(direction.getDegrees() - dir.getDegrees() > 180)
+            return 360 - direction.getDegrees();
+        if(direction.getDegrees() > dir.getDegrees()) {
+            return direction.getDegrees() - dir.getDegrees();
+        }
+        return dir.getDegrees() - direction.getDegrees();
     }
 }
